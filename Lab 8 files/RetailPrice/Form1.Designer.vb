1d4aecbf58c66451383bbc279ee102231b3158cc
Sam luyk <samluyk@users.noreply.github.com>
2018-03-10 15:51:02 -0500

    Add files via upload

---
+++ Lab 8 files/RetailPrice/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCalc
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCost = New System.Windows.Forms.TextBox()
+        Me.txtPercent = New System.Windows.Forms.TextBox()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtCost
+        '
+        Me.txtCost.Location = New System.Drawing.Point(171, 6)
+        Me.txtCost.Name = "txtCost"
+        Me.txtCost.Size = New System.Drawing.Size(100, 20)
+        Me.txtCost.TabIndex = 0
+        '
+        'txtPercent
+        '
+        Me.txtPercent.Location = New System.Drawing.Point(171, 35)
+        Me.txtPercent.Name = "txtPercent"
+        Me.txtPercent.Size = New System.Drawing.Size(100, 20)
+        Me.txtPercent.TabIndex = 1
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Location = New System.Drawing.Point(171, 63)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrice.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(84, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Wholesale Cost:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Markup %:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 66)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Retail Price:"
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(15, 99)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(81, 23)
+        Me.btnCalc.TabIndex = 6
+        Me.btnCalc.Text = "Get Retail"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(181, 99)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmCalc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(285, 157)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Controls.Add(Me.txtPercent)
+        Me.Controls.Add(Me.txtCost)
+        Me.Name = "frmCalc"
+        Me.Text = "Retail Price Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtCost As System.Windows.Forms.TextBox
+    Friend WithEvents txtPercent As System.Windows.Forms.TextBox
+    Friend WithEvents txtPrice As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnCalc As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Lab 8 files/RetailPrice/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class frmCalc
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        'Function CalculateRetail(ByVal Cost As Decimal, ByVal Percent As Decimal) As Decimal
+
+        Dim Cost As Decimal
+        Dim Percent As Decimal
+        Dim Price As Decimal
+
+        Cost = txtCost.Text
+        Percent = txtPercent.Text
+
+        If (Cost > 0) And (Percent > 0) Then
+            Percent = (1 + (txtPercent.Text / 100))
+            Price = Cost * Percent
+            txtPrice.Text = Price
+        Else
+            MessageBox.Show("That isn't a positive number, man.",
+            "User Input Error",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Error)
+        End If
+
+
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
+End Class
