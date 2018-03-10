bfdb59f5106a4bdf5ce244a00d38dc4b0417286b
Sam luyk <samluyk@users.noreply.github.com>
2018-03-10 15:41:27 -0500

    Add files via upload

---
+++ OvertimeCalc/Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPay
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtHours = New System.Windows.Forms.TextBox()
+        Me.txtRate = New System.Windows.Forms.TextBox()
+        Me.txtGross = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(51, 202)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 35)
+        Me.btnCalc.TabIndex = 4
+        Me.btnCalc.Text = "&Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(145, 202)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 35)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Hours Worked:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 81)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(87, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Hourly Pay Rate:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 132)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Gross Pay:"
+        '
+        'txtHours
+        '
+        Me.txtHours.Location = New System.Drawing.Point(135, 30)
+        Me.txtHours.Name = "txtHours"
+        Me.txtHours.Size = New System.Drawing.Size(100, 20)
+        Me.txtHours.TabIndex = 1
+        '
+        'txtRate
+        '
+        Me.txtRate.Location = New System.Drawing.Point(135, 78)
+        Me.txtRate.Name = "txtRate"
+        Me.txtRate.Size = New System.Drawing.Size(100, 20)
+        Me.txtRate.TabIndex = 2
+        '
+        'txtGross
+        '
+        Me.txtGross.Enabled = False
+        Me.txtGross.Location = New System.Drawing.Point(135, 132)
+        Me.txtGross.Name = "txtGross"
+        Me.txtGross.Size = New System.Drawing.Size(100, 20)
+        Me.txtGross.TabIndex = 3
+        '
+        'frmPay
+        '
+        Me.AcceptButton = Me.btnCalc
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.txtGross)
+        Me.Controls.Add(Me.txtRate)
+        Me.Controls.Add(Me.txtHours)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Name = "frmPay"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Gross Pay Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCalc As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtHours As System.Windows.Forms.TextBox
+    Friend WithEvents txtRate As System.Windows.Forms.TextBox
+    Friend WithEvents txtGross As System.Windows.Forms.TextBox
+
+End Class
---
+++ OvertimeCalc/Form1.vb
@@ -0,0 +1,116 @@
+﻿Public Class frmPay
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+
+        Const FULL_TIME As Integer = 40     '
+        Const MULTIPLIER As Decimal = 1.5D  '
+        Dim otRate As Decimal               '
+        Dim otPay As Decimal                '
+        Dim otHours As Integer              '
+        Dim numHours As Integer = 0         '
+        Dim payRate As Decimal = 0D         '
+        Dim grossPay As Decimal = 0D        '
+        Dim bonusAmt As Decimal = 0D        '
+        Dim strBonusAmt As String           '
+
+        Try
+
+            numHours = CInt(txtHours.Text)
+
+            If numHours < 0 Then
+
+
+                MessageBox.Show("The number of hours worked must be a positive number.",
+                "User Input Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+
+            End If
+
+
+            Try
+                payRate = CDec(txtRate.Text)
+
+                If payRate < 0 Then
+
+                    txtRate.Clear()
+                    txtRate.Focus()
+
+                    MessageBox.Show("The hourly rate must be positive.",
+                    "User Input Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+                Else
+
+
+
+                    If numHours > FULL_TIME Then
+                        'They earned OT Pay
+                        'Calc OT pay rate
+                        otRate = MULTIPLIER * payRate
+
+                        '# of hours OT is earned for
+                        otHours = numHours - FULL_TIME
+
+                        'Calc OT amt
+                        otPay = CDec(otHours) * otRate
+
+                        'calc gross pay
+                        grossPay = CDec(FULL_TIME) * payRate + otPay
+
+                    Else
+
+
+                        'Calc pay without OT
+                        grossPay = CDec(numHours) * payRate
+                    End If
+
+                    'Check for a bonus
+                    strBonusAmt = InputBox("Enter the bonus amount",
+                                           "Bonus")
+                    'Check for a valid bonus amount
+                    If (Decimal.TryParse(strBonusAmt, bonusAmt)) Then
+
+
+                        'Add in the gross pay amount
+                        grossPay += bonusAmt
+                    Else
+                        'Not a currency value
+                        MessageBox.Show("The bonus amount must be a currency value, or 0.",
+                        "User Input Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End If
+
+
+                    'output our result
+                    txtGross.Text = grossPay.ToString("c")
+                    End If
+
+
+            Catch ex As Exception
+
+                txtRate.Clear()
+                txtRate.Focus()
+                MessageBox.Show("The pay rate must be a currency value.",
+                "User Input Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+
+            End Try
+
+
+        Catch ex As Exception
+
+            txtHours.Clear()
+            txtHours.Focus()
+            MessageBox.Show("The number of hours worked must be a whole number.",
+                            "User Input Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+End Class
